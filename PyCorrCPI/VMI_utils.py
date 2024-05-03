2dec490e49deb5d0831734aa8156788d396082e7
Bjoern Senfftleben <bjoern.senfftleben@xfel.eu>
2024-05-03 13:43:59 +0200

    add class for tof calibration

--- PyCorrCPI/VMI_utils.py
+++ PyCorrCPI/VMI_utils.py
@@ -1,4 +1,6 @@
 import periodictable as pse
+from scipy.optimize import curve_fit
+from collections.abc import Iterable
 
 
 def parse_chemical_formula(chemical_formula:str)->dict:
@@ -36,7 +38,8 @@ def parse_chemical_formula(chemical_formula:str)->dict:
         
     counts = [int(c) for c in counts]
     return dict(zip(elements, counts))
-    
+
+
 def get_molecule_mass(chemical_formula:str):
     """
     gives mass in amu for any given chemical formula
@@ -45,7 +48,61 @@ def get_molecule_mass(chemical_formula:str):
     count_by_element = parse_chemical_formula(chemical_formula)
     return sum([getattr(pse, e).mass*count_by_element[e] for e in count_by_element])
 
+
 # function to parse ion strings -> i.e. [chemical formula]^[n]+
 get_ion_mass = lambda ion: get_molecule_mass(ion.split("^")[0])
 get_ion_charge = lambda ion: int(ion.split("^")[1][:-1])
 get_ion_mz = lambda ion: get_ion_mass(ion)/get_ion_charge(ion)
+
+
+class TofCalibration:
+    def __init__(self, coefficients=None):
+        self._coeffs = None
+        
+        if coefficients:
+            self.coeffs = coefficients
+        
+    @staticmethod
+    def _tof_to_mz(t, a, b):
+        return ((t - a) / b) ** 2
+    
+    @staticmethod
+    def _mz_to_tof(mz, a, b):
+        return np.sqrt(mz) * b + a
+    
+    @property
+    def coeffs(self):
+        return self._coeffs
+        
+    @coeffs.setter
+    def coeffs(self, value):
+        if hasattr(value, "__len__"):
+            if len(value) == 2:
+                self._coeffs = (float(value[0]), float(value[1]))
+                return 
+        raise ValueError(f"{value} is not valid for coeffs")
+        
+    @property
+    def t0(self):
+        if self.coeffs:
+            return self.coeffs[0]
+        return np.nan
+    
+    def tof_to_mz(self, t):
+        if not self.coeffs:
+            raise ValueError("calibration lacks coefficients (self.coeffs is None)")
+        return self._tof_to_mz(t, *self.coeffs)
+    
+    def mz_to_tof(self, mz):
+        if not self.coeffs:
+            raise ValueError("calibration lacks coefficients (self.coeffs is None)")
+        return self._mz_to_tof(mz, *self.coeffs)
+    
+    def calibrate(self, tofs:Iterable, mzs:Iterable):
+        popt, pcov = curve_fit( self._tof_to_mz, 
+                                tofs, 
+                                mzs)
+        
+        print(popt)
+        self.coeffs = popt
+        return self.coeffs, pcov
